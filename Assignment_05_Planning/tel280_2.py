c3bc84e92d512559fffa82613110606461fb66b4
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2022-11-14 20:09:28 +0100

    Fixed classes with free and goal and start

--- Assignment_05_Planning/tel280_2.py
+++ Assignment_05_Planning/tel280_2.py
@@ -1,74 +1,64 @@
 import skimage
 import matplotlib.pyplot as plt
-
-image = skimage.io.imread('Assignment5_tel280.png')
-
-
-class Squared:
+import numpy as np
+class Squared(object):
     def __init__(self, x_start, x_stop, y_start, y_stop):
         self.x_start = x_start
         self.x_stop = x_stop
         self.y_start = y_start
         self.y_stop = y_stop
-        self.free = False
+        self.free = True
         self.goal = False
         self.start = False
 
-    def check_for_collision(self):
-        for k in range(int(self.x_start), int(self.x_stop)):
-            for l in range(int(self.y_start), int(self.y_stop)):
-                if image[k][l][0] > 200:
-                    self.free = True
-        pass
+    def check_box(self, image):
+        for k in np.arange(self.x_start, self.x_stop, 1):
+            for l in np.arange(self.y_start, self.y_stop, 1):
+                if image[int(l), int(k), 0] > 200 and image[int(l), int(k), 1] < 1 and image[int(l), int(k), 2] < 1:
+                    self.free = False
+                    break;
 
-    def check_for_goal(self):
-        for k in range(int(self.x_start), int(self.x_stop)):
-            for l in range(int(self.y_start), int(self.y_stop)):
-                if image[k][l][1] > 200:
+                elif 100 < image[int(l), int(k), 1] > 200 and image[int(l), int(k), 0] < 200 and image[int(l), int(k), 2] < 200:
                     self.goal = True
-        pass
-    def check_for_start(self):
-        for k in range(int(self.x_start), int(self.x_stop)):
-            for l in range(int(self.y_start), int(self.y_stop)):
-                if image[k][l][3] > 200:
+                    break;
+
+                elif image[int(l), int(k), 2] > 200 and image[int(l), int(k), 0] < 200 and image[int(l), int(k), 1] < 200:
                     self.start = True
-        pass
+                    break;
 
 
-xstart = x__start = 0
-xstop = x__stop = 1169
-ystart = 0
-ystop = y__stop = 797
-grid_sizex = 0
-stepx = (xstop-xstart)/22
-grid_sizey = 0
-stepy = (ystop-ystart)/15
-x__stop = x__start + stepx
-for i in range(21):
-    x__start += stepx
-    x__stop += stepx
-    y__start = ystart
-    y__stop = y__start + stepy
-    for j in range(14):
-        y__start += stepy
-        y__stop += stepy
-        box = Squared(x__start, x__stop, y__start, y__stop)
-        print(x__start, x__stop, y__start, y__stop)
-        image[int(y__start):int(y__stop), int(x__start)] = 0
-        image[int(y__start):int(y__stop), int(x__stop)] = 0
-        image[int(y__start), int(x__start):int(x__stop)] = 0
-        image[int(y__start), int(x__stop):int(x__stop)] = 0
-"""
-for i in range(22):
-    image[int(y_start):int(y_stop), int(grid_sizex)] = 0
-    grid_sizex += (x_stop-x_start)/21
-    print(grid_sizex, grid_sizey)
+if __name__ == '__main__':
 
-for i in range(15):
-    image[int(grid_sizey), int(x_start):int(x_stop)] = 0
-    grid_sizey += (y_stop-y_start)/14
-    print(grid_sizex, grid_sizey)
+    im = skimage.io.imread('Assignment5_tel280.png')
 
-"""
-plt.imshow(image)
-plt.show()
+    xstart = x__start = 0
+    xstop = x__stop = 1169
+    ystart = 0
+    ystop = y__stop = 797
+    grid_sizex = 0
+    stepx = (xstop-xstart)/22
+    grid_sizey = 0
+    stepy = (ystop-ystart)/15
+    x__stop = x__start + stepx
+
+    list_of_nodes = []
+    for i in range(21):
+        x__start += stepx
+        x__stop += stepx
+        y__start = ystart
+        y__stop = y__start + stepy
+        for j in range(14):
+            y__start += stepy
+            y__stop += stepy
+            list_of_nodes.append(Squared(x__start, x__stop, y__start, y__stop))
+            im[int(y__start):int(y__stop), int(x__start)] = 0
+            im[int(y__start):int(y__stop), int(x__stop)] = 0
+            im[int(y__start), int(x__start):int(x__stop)] = 0
+            im[int(y__start), int(x__stop):int(x__stop)] = 0
+
+
+    for element, node in enumerate(list_of_nodes):
+        node.check_box(im)
+
+    plt.imshow(im)
+    plt.show()
